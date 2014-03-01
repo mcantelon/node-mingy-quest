f0687346be867f920a8b4f34b50d7a64957e2abc
Mike Cantelon <mcantelon@gmail.com>
2014-03-01 03:00:41 -0800

    More fleshing out.

--- eval.js
+++ eval.js
@@ -4,11 +4,25 @@ var mingyQuest = require('./lib/index')
   , Game = mingyQuest.Game
   , mingy = require('mingy')
   , Parser = mingy.Parser
+  , Command = mingy.Command
 
-var parser = new Parser()
+var command = new Command('go')
+command.set('syntax', ['go <direction>'])
+command.set('logic', function(args) {
+  if (!args || !args['direction']) {
+    return 'What?'
+  }
+  else {
+    return 'You go ' + args['direction'] + '.'
+  }
+})
+
+var parser = new Parser([command])
+var output = parser.parseLexemes(['go', 'north'])
+console.log('O:' + output)
 
 var game = new Game(parser)
 .addElementType('Location', __dirname + '/lib/location.js', 'locations')
 
-game.addElementFromYamlFile('Location', __dirname + '/games/daydream/locations/bedroom.yaml')
+game.addElementFromYamlFile('Location', __dirname + '/games/daydream/locations/entrance.yaml')
 console.log(game.locations);
